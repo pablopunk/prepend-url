a9a08e4f3c6880094b5c4f32c4467e5dc0bbb546
Pablo Varela <pablovarela182@gmail.com>
2017-09-26 10:56:11 +0200

    Death to semicolons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const url = require('url');
+const url = require('url')
 
 module.exports = (from, to) => {
-  const parsed = url.parse(from);
-  return `${to}/${parsed.host}${parsed.path}`;
-};
+  const parsed = url.parse(from)
+  return `${to}/${parsed.host}${parsed.path}`
+}
