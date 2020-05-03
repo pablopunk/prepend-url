e0d93a3e6ef608710783794a140e901af460c55f
pablopunk <pablovarela182@gmail.com>
2020-05-03 20:11:33 +0200

    ava -> myass and standard -> prettier

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-const test = require('ava')
+const test = require('myass')
 const prepend = require('.')
 
-test('prepend url', t => {
+test('prepend url', (t) => {
   const result = prepend('http://pablopunk.com', 'https://microsec.pw')
   const expected = 'https://microsec.pw/pablopunk.com/'
   t.is(result, expected)
 })
 
-test('returns same url if nothing is passed', t => {
+test('returns same url if nothing is passed', (t) => {
   const result = prepend('http://pablopunk.com', '')
   const expected = 'http://pablopunk.com'
   t.is(result, expected)
 })
 
-test('returns same url if null is passed', t => {
+test('returns same url if null is passed', (t) => {
   const result = prepend('http://pablopunk.com', null)
   const expected = 'http://pablopunk.com'
   t.is(result, expected)
 })
 
-test('returns new url if the first parameter is nothing', t => {
+test('returns new url if the first parameter is nothing', (t) => {
   const result = prepend('', 'https://microsec.pw')
   const expected = 'https://microsec.pw/'
   t.is(result, expected)
